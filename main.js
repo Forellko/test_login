1cda6e69ed222d4473af005720f1d74f22b93f82
Forellko <dragonforce97q@gmail.com>
2018-12-11 17:43:10 +0300

    Added new page and user: login-admin,password-admin.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 function doSub(){
   let a = document.getElementById("logint").value;
   let b = document.getElementById("passwordt").value;
-  localStorage.setItem("login", a);
-  localStorage.setItem("password", b);
+  if(localStorage.getItem("login") == a){
+    if(localStorage.getItem("password") == b){
+      window.open("new.html");
+    }else{
+      alert("Wrong login or password");
+      return;
+    }
+  }else{
+    alert("Wrong login or password");
+  }
 }
 document.getElementById("submitt").onclick = doSub;
+
+localStorage.setItem("login", "admin");
+localStorage.setItem("password", "admin");
