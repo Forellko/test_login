35fd26c5ffcff627b0cc67c2817e75f9097d876f
Forellko <dragonforce97q@gmail.com>
2018-12-11 17:19:42 +0300

    Added data to localstorage.

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-//let a = document.getElementById("login");
+function doSub(){
+  let a = document.getElementById("logint").value;
+  let b = document.getElementById("passwordt").value;
+  localStorage.setItem("login", a);
+  localStorage.setItem("password", b);
+}
+document.getElementById("submitt").onclick = doSub;
