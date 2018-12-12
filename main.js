23dda65c34007c630e1369c775a39380efbc6ac1
Forellko <dragonforce97q@gmail.com>
2018-12-12 11:26:41 +0300

    Added new page if submit with login="admin" and password="admin"

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ function doSub(){
   let b = document.getElementById("passwordt").value;
   if(localStorage.getItem("login") == a){
     if(localStorage.getItem("password") == b){
-      window.open("new.html");
+      
     }else{
       alert("Wrong login or password");
-      return;
     }
   }else{
     alert("Wrong login or password");
   }
+  return;
 }
 document.getElementById("submitt").onclick = doSub;
 
---
+++ new.js
@@ -0,0 +1,4 @@
+function lock(){
+  a = localStorage.getItem("login1");
+  b = localStorage.getItem("password1");
+}
